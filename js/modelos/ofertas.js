3dfb1a7004ce7a0c6fd335dc004a9eef63bb0064
wvaldiviezo <wvaldiviezo23@gmail.com>
2016-07-26 14:26:58 -0500

    modelo sin relacion de ofertas

---
+++ js/modelos/ofertas.js
@@ -0,0 +1,37 @@
+module.exports = {
+  attributes: {
+    areaTrabajo: {
+      type: 'string',
+      required:true
+    },
+    tipoPuestoTrabajo: {
+      type: 'string',
+      required:true
+    },
+    facultadTrabajo: {
+      type: 'string',
+      required:true
+    },
+    remuneracion: {
+      type: 'float',
+      required:true
+    },
+    direccionTrabajo: {
+      type: 'string',
+      required:true
+    },
+    descripcionTrabajo: {
+      type: 'string',
+      required:true
+    },
+    fechaInicioPublicacionTrabajo: {
+      type: 'date',
+      required:true
+    },
+    fechaFinPublicacionTrabajo: {
+      type: 'date',
+      required:true
+    }
+
+    }
+};

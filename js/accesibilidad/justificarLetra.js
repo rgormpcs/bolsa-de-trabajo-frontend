22a1bf5d5a8f7a0a6fefe2051eddb1bb29d91145
rgormpcs <ricardo.mpcs@gmail.com>
2016-08-02 16:33:27 -0500

    funcion justificar texto

---
+++ js/accesibilidad/justificarLetra.js
@@ -0,0 +1,9 @@
+function justificar() {
+    // document.getElementById("myP").style.textAlign = "justify";
+    document.body.style.textAlign = "justify";
+}
+
+function noJustificar() {
+    //document.getElementById("myP").style.textAlign = "left";
+    document.body.style.textAlign = "left";
+}

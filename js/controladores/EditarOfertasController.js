6e2db0ce19b98c64375321a3a7c79186bdb1be16
hdcrldu55 <hdcrldu55@gmail.com>
2016-08-04 10:00:53 -0500

    editar oferta

--- js/controladores/EditarOfertasController.js
+++ js/controladores/EditarOfertasController.js
@@ -1,36 +1,38 @@
-app.controller('EditarOfertaCtrl', ['$scope', 'OfertaTrabajoFactory', '$cookies', function($scope, OfertaTrabajoFactory, $cookies) {
+app.controller('EditarOfertaCtrl', ['$scope', '$stateParams', 'OfertaTrabajoFactory', 'EmpleadorFactory', '$cookies', function($scope, $stateParams, OfertaTrabajoFactory, EmpleadorFactory, $cookies) {
     // $scope.empleador = {};
+console.log("id empelador vaaaaaa???",$stateParams.idEmpleador);
 
-OfertaTrabajoFactory.getOfertaTrabajoPorEmpleador({
-   empleadorId: $cookies.get('idOfertaTrabajo')
-}).$promise.then(
-    function success(respuesta) {
-        console.log("ofertaTrabajo", respuesta);
-        $scope.ofertaTrabajo = respuesta;
-    },
-    function error(error) {
-        console.log(error);
-    });
+    OfertaTrabajoFactory.getOfertaTrabajoPorEmpleador({
+        //idEmpleador: $stateParams.idEmpleador
+        idEmpleador: $cookies.get('empleadorId')
+    }).$promise.then(
+        function success(respuesta) {
+            console.log("ofertaTrabajo", respuesta);
+            $scope.ofertaTrabajo = respuesta;
+        },
+        function error(error) {
+            console.log(error);
+        });
 
     $scope.editarOferta = function() {
         console.log('ofertaTrabajo', $scope.ofertaTrabajo);
         OfertaTrabajoFactory.updateOferta({
             idOfertaTrabajo: $cookies.get('OfertaTrabajoId')
         }, {
-          areaTrabajo: $scope.ofertaTrabajo.areaTrabajo,
-    tipoDePuestoTrabajo: $scope.ofertaTrabajo.tipoDePuestoTrabajo,
-    facultadTrabajo: $scope.ofertaTrabajo.facultadTrabajo,
-    remuneracion: $scope.ofertaTrabajo.remuneracion,
-    direccionTrabajo: $scope.ofertaTrabajo.direccionTrabajo,
-    descripcionTrabajo: $scope.ofertaTrabajo.descripcionTrabajo,
-    fechaInicioPublicacionTrabajo: $scope.ofertaTrabajo.fechaInicioPublicacionTrabajo,
-    fechaFinPublicacionTrabajo: $scope.ofertaTrabajo.fechaFinPublicacionTrabajo,
+            areaTrabajo: $scope.ofertaTrabajo.areaTrabajo,
+            tipoDePuestoTrabajo: $scope.ofertaTrabajo.tipoDePuestoTrabajo,
+            facultadTrabajo: $scope.ofertaTrabajo.facultadTrabajo,
+            remuneracion: $scope.ofertaTrabajo.remuneracion,
+            direccionTrabajo: $scope.ofertaTrabajo.direccionTrabajo,
+            descripcionTrabajo: $scope.ofertaTrabajo.descripcionTrabajo,
+            fechaInicioPublicacionTrabajo: $scope.ofertaTrabajo.fechaInicioPublicacionTrabajo,
+            fechaFinPublicacionTrabajo: $scope.ofertaTrabajo.fechaFinPublicacionTrabajo,
 
         }).$promise.then(
             function success(respuesta) {
                 console.log("oferta actualizado", respuesta);
                 $scope.ofertaTrabajo = respuesta;
-                 oferta.mostrar = !oferta.mostrar
+                oferta.mostrar = !oferta.mostrar
             },
             function error(error) {
                 console.log(error);
--- js/factories/OfertaTrabajoFactory.js
+++ js/factories/OfertaTrabajoFactory.js
@@ -1,28 +1,29 @@
 app.factory('OfertaTrabajoFactory', ['$resource', function($resource) {
-    var factory = $resource('http://localhost:1337/ofertaTrabajo/:idOfertaTrabajo', {
-        idOfertaTrabajo: '@idOfertaTrabajo'
-    }, {
-        registrarNuevaOfertaBE: {
-            url: 'http://localhost:1337/ofertaTrabajo',
-            method: 'POST',
-            params: {
-                idEmpleador: '@idEmpleador'
-            }
-        },
-        getOfertaTrabajoPorEmpleador: {
-            url: 'http://localhost:1337/Empleador/getOfertaTrabajoPorEmpleador',
-            method: 'GET',
-            params: {
-                idOfertaTrabajo: '@idOfertaTrabajo'
-            }
-        },
-        updateOferta: {
-            url: 'http://localhost:1337/Empleador/updateOferta',
-            method: 'PUT',
-            params: {
-                idOfertaTrabajo: '@idOfertaTrabajo'
-            }
+var factory = $resource('http://localhost:1337/ofertaTrabajo/:idOfertaTrabajo', {
+            idOfertaTrabajo: '@idOfertaTrabajo'
+        }, {
+            registrarNuevaOfertaBE: {
+                url: 'http://localhost:1337/ofertaTrabajo',
+                method: 'POST',
+                params: {
+                    idEmpleador: '@idEmpleador'
+                }
+            },
+            getOfertaTrabajoPorEmpleador: {
+                url: 'http://localhost:1337/ofertaTrabajo?idEmpleador=:idEmpleador',
+                method: 'GET',
+                params: {
+                    idEmpleador: '@idEmpleador'
+                },
+                    isArray: true
+    },
+    updateOferta: {
+        url: 'http://localhost:1337/Empleador/updateOferta',
+        method: 'PUT',
+        params: {
+            idOfertaTrabajo: '@idOfertaTrabajo'
         }
-    });
-    return factory;
+    }
+});
+return factory;
 }]);

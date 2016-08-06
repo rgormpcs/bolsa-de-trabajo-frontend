f9d6c8b86320e9f60ec9e450ad4c9bfe85d35cb0
rgormpcs <ricardo.mpcs@gmail.com>
2016-08-05 19:17:59 -0500

    correccioncontroler postulante

--- js/controladores/RegistroDePostulanteController.js
+++ js/controladores/RegistroDePostulanteController.js
@@ -1,11 +1,10 @@
-app.controller('RegistroDePostulanteCtrl', ['$scope','$state',  'PostulanteFactory', function($scope, PostulanteFactory) {
+app.controller('RegistroDePostulanteCtrl', ['$scope', 'PostulanteFactory', function($scope, PostulanteFactory) {
     $scope.postulante = {};
     $scope.registrarPostulante = function() {
       console.log('postulante', $scope.postulante);
         PostulanteFactory.save($scope.postulante).$promise.then(function success(respuesta) {
                 console.log('éxito en ingreso de solicitud', respuesta);
                 $scope.postulante = {};
-                $state.go('login');
             },
             function error(error) {
                 console.log('Error en ingreso de solicitud', error);

d983ae84a48443ab63602d828a5dd680be0fe4ed
root <root@localhost.localdomain>
2016-08-07 18:11:28 -0500

    cambio de titulos con la etiqueta h no se aplica el estilo

--- js/accesibilidad/cambiarLetra.js
+++ js/accesibilidad/cambiarLetra.js
@@ -1,7 +1,7 @@
 
 function san() {
   console.log("entro la funcion");
-     document.getElementById("wrapper").style.fontFamily = "Impact,Charcoal,sans-serif";
+     document.getElementById("wrapper").style.fontFamily= "Impact,Charcoal,sans-serif";
     //document.body.style.fontFamily = "Impact,Charcoal,sans-serif";
 
 

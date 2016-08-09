d364bc715ffe35c0ff90256cb48e5dcfc80c0001
hdcrldu55 <hdcrldu55@gmail.com>
2016-08-09 11:35:34 -0500

    Cv postulante

--- js/controladores/PostulacionesPostulanteController.js
+++ js/controladores/PostulacionesPostulanteController.js
@@ -1,13 +1,25 @@
-app.controller('PostulacionesPostulanteCtrl', ['$scope', 'OfertaTrabajoFactory',
-    function($scope, OfertaTrabajoFactory) {
+app.controller('PostulacionesPostulanteCtrl', ['$scope','$cookies', 'OfertaTrabajoFactory', 'PostulanteFactory',
+    function($scope, $cookies, OfertaTrabajoFactory, PostulanteFactory) {
         OfertaTrabajoFactory.query().$promise.then(
             function success(respuesta) {
                 console.log("ofertas", respuesta);
                 $scope.ofertas = respuesta;
-                
+
             },
             function error(error) {
                 console.log(error);
             });
+
+            PostulanteFactory.getPostulante({
+                idPostulante: $cookies.get('postulanteId')
+            }).$promise.then(
+                function success(respuesta) {
+                    console.log("postulantes", respuesta);
+                    $scope.postulantes = respuesta;
+                },
+                function error(error) {
+                    console.log(error);
+                });
     }
+
 ]);

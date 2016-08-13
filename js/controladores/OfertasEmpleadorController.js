d746abc25a9c82f425d560c44a457e785ec2cf5a
LAPCRimay <lorepadilla0@gmail.com>
2016-08-12 22:21:34 -0500

    Filtrado de Ofertas (rol Empleador) por Facultad

--- js/controladores/OfertasEmpleadorController.js
+++ js/controladores/OfertasEmpleadorController.js
@@ -1,37 +1,4 @@
 app.controller('OfertasEmpleadorCtrl', ['$scope', '$state', '$cookies', 'OfertaTrabajoFactory', function($scope, $state, $cookies, OfertaTrabajoFactory) {
-  /*  $scope.facultades = [{
-        clave: 'ciencias',
-        nombre: 'Facultad de Ciencias'
-    }, {
-        clave: 'administrativas',
-        nombre: 'Facultad de Ciencias Administrativas'
-    }, {
-        clave: 'civil-ambiental',
-        nombre: 'Facultad de Ingeniería Civil y Ambiental'
-    }, {
-        clave: 'electrica-electronica',
-        nombre: 'Facultad de Ingeniería Elétrica y Eléctronica'
-    }, {
-        clave: 'geologia-petroleos',
-        nombre: 'Facultad de Ingeniería Geología y Petroleos'
-    }, {
-        clave: 'mecanica',
-        nombre: 'Facultad de Ingeniría Mecánica'
-    }, {
-        clave: 'sistemas',
-        nombre: 'Facultad de Ingeniería de Sistemas'
-    }, {
-        clave: 'quimica-agro',
-        nombre: 'Facultad de Ingeniería Química y Agroindustría'
-    }, {
-        clave: 'esfot',
-        nombre: 'ESFOT'
-    }, {
-        clave: 'prepo',
-        nombre: 'Formación Básica'
-    }];*/
-
-    // $scope.ofertas = [];
 
     OfertaTrabajoFactory.getOfertaTrabajoPorEmpleador({
         idEmpleador: $cookies.get('empleadorId')
@@ -43,4 +10,17 @@ app.controller('OfertasEmpleadorCtrl', ['$scope', '$state', '$cookies', 'OfertaT
         function error(error) {
             console.log(error);
         });
+
+    $scope.getOfertasPorFacultad = function(facultad) {
+        OfertaTrabajoFactory.getOfertaTrabajoPorFacultad({
+            facultadTrabajo: facultad
+        }).$promise.then(
+            function success(respuesta) {
+                console.log("OFERTAS", respuesta);
+                $scope.ofertas = respuesta;
+            },
+            function error(error) {
+                console.log(error);
+            });
+    }
 }]);

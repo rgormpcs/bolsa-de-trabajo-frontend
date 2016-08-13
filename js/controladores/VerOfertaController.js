7c1acd554e95bea1e4c20613093141a204995f8e
LAPCRimay <lorepadilla0@gmail.com>
2016-08-12 21:07:31 -0500

    Validación Postulaciones
    Si una postulación ya existe no se podrá registrar otra.

--- js/controladores/VerOfertaController.js
+++ js/controladores/VerOfertaController.js
@@ -1,6 +1,6 @@
-app.controller('VerOfertaCtrl', ['$scope', 'OfertaTrabajoFactory', '$stateParams', 'PostulacionFactory', '$cookies',
+app.controller('VerOfertaCtrl', ['$scope', '$rootScope', 'OfertaTrabajoFactory', '$stateParams', 'PostulacionFactory', '$cookies',
 
-    function($scope, OfertaTrabajoFactory, $stateParams, PostulacionFactory, $cookies) {
+    function($scope, $rootScope, OfertaTrabajoFactory, $stateParams, PostulacionFactory, $cookies) {
         OfertaTrabajoFactory.get({
             id: $stateParams.idOferta
         }).$promise.then(
@@ -12,19 +12,58 @@ app.controller('VerOfertaCtrl', ['$scope', 'OfertaTrabajoFactory', '$stateParams
                 console.log(error);
             });
 
-        $scope.postular = function() {
-            PostulacionFactory.save({
+        $scope.existePostulacion = function() {
+            //  console.log("HAY POSTULACION es",$scope.hayPostulacion)
+            OfertaTrabajoFactory.getPostulacion({
                 idPostulante: $cookies.get('postulanteId'),
                 idOfertaTrabajo: $stateParams.idOferta
             }).$promise.then(
                 function success(respuesta) {
-                    console.log("postulacion ok", respuesta);
-                      alert("Se Postulo Correctamente")
-                          location.href = "#/postulaciones-realizadas";
+                    if (respuesta.length >= 1) {
+                        //$scope.hayPostulacion = true;
+                        alert("Usted ya ha postulado para esta oferta");
+                    } else {
+                        //$scope.hayPostulacion = false;
+                        PostulacionFactory.save({
+                            idPostulante: $cookies.get('postulanteId'),
+                            idOfertaTrabajo: $stateParams.idOferta
+                        }).$promise.then(
+                            function success(respuesta) {
+                                console.log("postulacion ok", respuesta);
+                                alert("Su postulación ha sido registrada exitosamente")
+                                location.href = "#/postulaciones-realizadas";
+                            },
+                            function error(error) {
+                                console.log(error);
+                            });
+                    }
                 },
                 function error(error) {
                     console.log(error);
                 });
+            return $rootScope.existe = $scope.hayPostulacion;
         }
     }
 ]);
+/*  $scope.postular = function() {
+              $scope.existePostulacion($stateParams.idOferta);
+                if ( $rootScope.existe== false) {
+                    //  if ($scope.existe == false) {
+                    PostulacionFactory.save({
+                        idPostulante: $cookies.get('postulanteId'),
+                        idOfertaTrabajo: $stateParams.idOferta
+                    }).$promise.then(
+                        function success(respuesta) {
+                            console.log("postulacion ok", respuesta);
+                            alert("Se Postulo Correctamente")
+                            location.href = "#/postulaciones-realizadas";
+                        },
+                        function error(error) {
+                            console.log(error);
+                        });
+                } else {
+                    alert("no se puede postular");
+                }
+                console.log("IMPRESION DE VARIABLE EXISTE", $scope.hayPostulacion)
+            }
+    }*/
